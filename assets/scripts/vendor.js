b0a7c9f4b7968a62e2a644d64e35e25023cfba5d
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-11-26 13:20:48 -0500

    Updated vendor.js

---
+++ assets/scripts/vendor.js
@@ -0,0 +1,11 @@
+// ID's of images stored
+const project1Desc = document.getElementById("project1");
+const project2Desc = document.getElementById("project2");
+const project3Desc = document.getElementById("project3");
+const project4Desc = document.getElementById("project4");
+const project5Desc = document.getElementById("project5");
+const project6Desc = document.getElementById("project6");
+
+function setText() {
+    console.log();
+}
