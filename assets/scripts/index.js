5dcec7def7ff1e8a0c0559a19ba5fa58bf0fca95
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-01 15:21:23 -0500

    Updated index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,14 +1,25 @@
-// ID's of images stored
-const project1Desc = document.getElementById("project1");
-const project2Desc = document.getElementById("project2");
-const project3Desc = document.getElementById("project3");
-const project4Desc = document.getElementById("project4");
-const project5Desc = document.getElementById("project5");
-const project6Desc = document.getElementById("project6");
+/* 
+Handles mobile menu and descriptions of project displays
+*/
+
+
+
+//ID's of images stored
+const project1 = document.getElementById("project1");
+const project2 = document.getElementById("project2");
+const project3 = document.getElementById("project3");
+const project4 = document.getElementById("project4");
+const project5 = document.getElementById("project5");
+const project6 = document.getElementById("project6");
+
+//Base variables
+let projectDesc = "";
 
 //remove this later
 console.log("Page Loaded");
 
-function setText() {
+
+
+function setText(projectChoice) {
     console.log();
 }
