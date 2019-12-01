990e3950cf83d54c84a6835d0795caab0877970b
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-01 15:34:59 -0500

    Update index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,24 +2,42 @@
 Handles mobile menu and descriptions of project displays
 */
 
-
-
-//ID's of images stored
-const project1 = document.getElementById("project1");
-const project2 = document.getElementById("project2");
-const project3 = document.getElementById("project3");
-const project4 = document.getElementById("project4");
-const project5 = document.getElementById("project5");
-const project6 = document.getElementById("project6");
+//Event listeners
+document.querySelector("#project1").addEventListener("click", project1Box);
+document.querySelector("#project2").addEventListener("click", project2Box);
+document.querySelector("#project3").addEventListener("click", project3Box);
+document.querySelector("#project4").addEventListener("click", project4Box);
+document.querySelector("#project5").addEventListener("click", project5Box);
+document.querySelector("#project6").addEventListener("click", project6Box);
 
 //Base variables
 let projectDesc = "";
 
-//remove this later
-console.log("Page Loaded");
 
+function project1Box() {
+    console.log("project1 pressed");
+}
 
+function project2Box() {
+    console.log("project2 pressed");
+}
+
+function project3Box() {
+    console.log("project3 pressed");
+}
+
+function project4Box() {
+    console.log("project4 pressed");
+}
+
+function project5Box() {
+    console.log("project5 pressed");
+}
+
+function project6Box() {
+    console.log("project6 pressed");
+}
 
 function setText(projectChoice) {
-    console.log();
+    
 }
