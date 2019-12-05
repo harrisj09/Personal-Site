ec3740a6e2ad1a8f21230bf1730eb3434ad5f721
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-05 11:38:40 -0500

    Update index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,22 +1,44 @@
-//default values
-let type = "Both";
-let lang = "All";
+//Type
+const BOTH = 'BOTH';
+const PERSONAL = 'PERSONAL';
+const CLIENT = 'CLIENT';
+
+//Language
+const ALL = 'ALL';
+const HTML = 'HTML/CSS';
+const JAVASCRIPT = 'JAVASCRIPT';
+const WORDPRESS = 'WORDPRESS';
+const REACT = 'REACT';
+
+//Base variables for filtering results
+let projectType;
+let projectLang;
+
+//Apply this to the instance variables it doesn't apply to
+const NULL = null;
 
 //Element 
 let previousClick = document.getElementById('both');
 let previousClickLang = document.getElementById('all');
 
 //Create objects of the projects the actual version on my site will contain another parameter for a link for anchor tags 
-function project(name, lang, desc, type) {
+function project(name, desc, both, personal, client, all, html, js, wordpress, react) {
   this.name = name;
-  this.lang = lang;
   this.desc = desc;
-  this.type = type;
+  this.both = both;
+  this.personal = personal;
+  this.client = client;
+  this.all = all;
+  this.html = html;
+  this.js = js;
+  this.wordpress = wordpress;
+  this.react = react;
 }
 
 //Store objects
 const projects = [
-  new project("Job Listing", "HTML/CSS", "Static Page Containing Job Listings", "Personal"),
+    // name, desc, both,  personal, client, all, html, js, wordpress, react
+  new project("Job Listing", "Static Page Containing Job Listings", BOTH, PERSONAL, NULL, ALL, HTML, ),
   new project("Calculator", "HTML/CSS, JavaScript", "Basic JavaScript Calculator", "Personal"),
   new project("TODO", "HTML/CSS, JavaScript, React", "Organize your everyday life with this web app made with React", "Personal"),
   new project("Login and Signup", "HTML/CSS", "Static Page for logging in and signing up", "Personal"),
@@ -48,7 +70,11 @@ function languageFilter(obj, string) {
 //Checks what the variables type and lang have and filters out if that object contains that word
 //I should probably make another array for the projects boxes (the outline)
 function projectFilter() {
-  let i = 0;
+  const typeFilter
+
+}
+
+/*
   const isValid = (projects[i].type == type && projects[i].lang == lang);
   //For loop starts checks base variables.
   for(i = 0; i < projects.length; ++i) {
@@ -56,4 +82,4 @@ function projectFilter() {
         //Logic goes here
       }
   }
-}
+*/
