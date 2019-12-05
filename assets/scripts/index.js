6da8d68f8498ec99705006eb719ae832a02dc324
John Harris <vosm803@gmail.com>
2019-12-05 09:51:08 -0500

    Update index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,9 +6,6 @@ let lang = "All";
 let previousClick = document.getElementById('both');
 let previousClickLang = document.getElementById('all');
 
-//stores the objects
-let projectsArray = [];
-
 //Create objects of the projects the actual version on my site will contain another parameter for a link for anchor tags 
 function project(name, lang, desc, type) {
   this.name = name;
@@ -17,20 +14,15 @@ function project(name, lang, desc, type) {
   this.type = type;
 }
 
-//Project Objects
-let project1 = new project("Job Listing", "HTML/CSS", "Static Page Containing Job Listings", "Personal");
-let project2 = new project("Calculator", "HTML/CSS, JavaScript", "Basic JavaScript Calculator", "Personal");
-let project3 = new project("TODO", "HTML/CSS, JavaScript, React", "Organize your everyday life with this web app made with React", "Personal");
-let project4 = new project("Login and Signup", "HTML/CSS", "Static Page for logging in and signing up", "Personal");
-let project5 = new project("RNG Therapy Site", "WordPress", "WordPress Site that allows people to schedule appointments.", "Client");
-let project6 = new project("Coffee Shop Website", "HTML/CSS, JavaScript, React", "Static Page for a Coffee Shop", "Client");
-
-projectsArray.push(project1);
-projectsArray.push(project2);
-projectsArray.push(project3);
-projectsArray.push(project4);
-projectsArray.push(project5);
-projectsArray.push(project6);
+//Store objects
+const projects = [
+  new project("Job Listing", "HTML/CSS", "Static Page Containing Job Listings", "Personal"),
+  new project("Calculator", "HTML/CSS, JavaScript", "Basic JavaScript Calculator", "Personal"),
+  new project("TODO", "HTML/CSS, JavaScript, React", "Organize your everyday life with this web app made with React", "Personal"),
+  new project("Login and Signup", "HTML/CSS", "Static Page for logging in and signing up", "Personal"),
+  new project("RNG Therapy Site", "WordPress", "WordPress Site that allows people to schedule appointments.", "Client"),
+  new project("Coffee Shop Website", "HTML/CSS, JavaScript, React", "Static Page for a Coffee Shop", "Client"),
+];
 
 //Handles what color changes calls filter function at the end to automatically update results
 function typeFilter(obj, string) {
@@ -56,5 +48,12 @@ function languageFilter(obj, string) {
 //Checks what the variables type and lang have and filters out if that object contains that word
 //I should probably make another array for the projects boxes (the outline)
 function projectFilter() {
-  
+  let i = 0;
+  const isValid = (projects[i].type == type && projects[i].lang == lang);
+  //For loop starts checks base variables.
+  for(i = 0; i < projects.length; ++i) {
+      if(isValid) {
+        //Logic goes here
+      }
+  }
 }
