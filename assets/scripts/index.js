bfca924547193572ad5863d0360e5353fc4e312b
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-06 19:22:36 -0500

    updated index,js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,39 +9,33 @@ const JAVASCRIPT = 'JAVASCRIPT';
 const WORDPRESS = 'WORDPRESS';
 const REACT = 'REACT';
 
-//Apply this to the instance variables it doesn't apply to
-const NULL = null;
-
 //Base variables for filtering results
-let projectType;
-let projectLang;
+let projectType = BOTH;
+let projectLang = ALL;
+let filteredArray = [];
 
 //Element make this blank and check if its null every time in the functions that handle this. If its null dont do anything otherwise change its colors
 let previousClickType = document.getElementById('both');
 let previousClickLang = document.getElementById('all');
 
 //Create objects of the projects the actual version on my site will contain another parameter for a link for anchor tags 
-function project(name, desc, both, personal, client, all, html, wordpress, js, react) {
+function project(name, desc, both, type, all, language) {
   this.name = name;
   this.desc = desc;
   this.both = both;
-  this.personal = personal;
-  this.client = client;
+  this.type = type;
   this.all = all;
-  this.html = html;
-  this.wordpress = wordpress;
-  this.js = js;
-  this.react = react;
+  this.language = language;
 }
 
 //Store objects
 const projects = [
-  new project("Job Listing", "Static Page Containing Job Listings", BOTH, PERSONAL, NULL, ALL, HTML, NULL, NULL, NULL),
-  new project("Calculator", "Basic JavaScript Calculator", BOTH, PERSONAL, NULL, ALL, HTML, NULL, JAVASCRIPT, NULL),
-  new project("TODO", "Organize your everyday life with this web app made with React", BOTH, PERSONAL, NULL, ALL, HTML, NULL, JAVASCRIPT, REACT),
-  new project("Login and Signup", "Static Page for logging in and signing up", BOTH, PERSONAL, NULL, ALL, HTML, NULL, NULL, NULL),
-  new project("RNG Therapy Site", "WordPress Site that allows people to schedule appointments.", BOTH, NULL, CLIENT, ALL, NULL, WORDPRESS, NULL, NULL),
-  new project("Coffee Shop Website","Static Page for a Coffee Shop", BOTH, NULL, CLIENT, ALL, HTML, NULL, JAVASCRIPT, REACT),
+    new project("Job Listing", "Static Page Containing Job Listings", BOTH, PERSONAL, ALL, HTML),
+    new project("Calculator", "Basic JavaScript Calculator", BOTH, PERSONAL, ALL, HTML + ", " + JAVASCRIPT),
+    new project("TODO", "Organize your everyday life with this web app made with React", BOTH, PERSONAL, ALL, HTML + ", " + JAVASCRIPT + ", " + REACT),
+    new project("Login and Signup", "Static Page for logging in and signing up", BOTH, PERSONAL, ALL, HTML),
+    new project("RNG Therapy Site", "WordPress Site that allows people to schedule appointments.", BOTH, CLIENT, ALL, WORDPRESS),
+    new project("Coffee Shop Website","Static Page for a Coffee Shop", BOTH, CLIENT, ALL, HTML + ", " + JAVASCRIPT + ", " + REACT),
 ];
 
 //Handles what color changes calls filter function at the end to automatically update results
@@ -52,8 +46,7 @@ function typeFilter(obj, string) {
     obj.style.color = "white";
     previousClickType = obj;
     projectType = string;
-    projects.filter(filterArray);
-    console.log(projects);
+    projects.filter(finalFilter);
 }
 
 function languageFilter(obj, string) {
@@ -63,11 +56,11 @@ function languageFilter(obj, string) {
     obj.style.color = "white";
     previousClickLang = obj;
     projectLang = string;
-    projects.filter(filterArray);
-    console.log(projects);
+    projects.filter(finalFilter);
 }
 
-function filterArray() {
-  //check if theyre not null.
-    return projects.indexOf(projectType) !== -1 && projects.indexOf(projectLang) !== -1;
+//if projects.includes(projectType) push array into new array
+function finalFilter() {
+    //Have logic here for when the selection is all or both
+    return projects.type.includes(projectType) && projects.language.includes(projectLang);
 }
