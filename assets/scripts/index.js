2dd632398e30c77fa2071d24a67ab0fc3569f853
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-09 18:52:08 -0500

    Update index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -23,7 +23,9 @@ let projectLang = ALL;
 
 // Get rid of the onClick in the html and do it all in here.
 // Give them a shared class so you can select all of them at once
-// const btnOptions = document.querySelectorAll('.sharedBtns');   (Terrible naming! ;P)
+// const btnOptions = document.querySelectorAll('.sharedType');
+
+
 /*
     now you can simply loop over them and add an eventListener:
     btnOptions.forEach(button => button.addEventListener('click', callback)
@@ -32,22 +34,20 @@ let previousClickType = document.getElementById('both');
 let previousClickLang = document.getElementById('all');
 
 
-function project(name, desc, both, type, all, language) {
+function project(name, desc, type, language) {
     this.name = name;
     this.desc = desc;
-    this.both = both;
     this.type = type;
-    this.all = all;
     this.language = language;
 }
 
 const projects = [
-    new project("Job Listing", "Static Page Containing Job Listings", BOTH, PERSONAL, ALL, HTML),
-    new project("Calculator", "Basic JavaScript Calculator", BOTH, PERSONAL, ALL, HTML + ", " + JAVASCRIPT),
-    new project("TODO", "Organize your everyday life with this web app made with React", BOTH, PERSONAL, ALL, HTML + ", " + JAVASCRIPT + ", " + REACT),
-    new project("Login and Signup", "Static Page for logging in and signing up", BOTH, PERSONAL, ALL, HTML),
-    new project("RNG Therapy Site", "WordPress Site that allows people to schedule appointments.", BOTH, CLIENT, ALL, WORDPRESS),
-    new project("Coffee Shop Website","Static Page for a Coffee Shop", BOTH, CLIENT, ALL, HTML + ", " + JAVASCRIPT + ", " + REACT),
+    new project("Job Listing", "Static Page Containing Job Listings", PERSONAL, HTML),
+    new project("Calculator", "Basic JavaScript Calculator", PERSONAL, HTML + ", " + JAVASCRIPT),
+    new project("TODO", "Organize your everyday life with this web app made with React", PERSONAL, HTML + ", " + JAVASCRIPT + ", " + REACT),
+    new project("Login and Signup", "Static Page for logging in and signing up", PERSONAL, HTML),
+    new project("RNG Therapy Site", "WordPress Site that allows people to schedule appointments.", CLIENT, WORDPRESS),
+    new project("Coffee Shop Website","Static Page for a Coffee Shop", CLIENT, HTML + ", " + JAVASCRIPT + ", " + REACT),
 ];
 
 // Make css classes, and add/remove classes based on the STATE. Instead of directly changing it's property values
@@ -59,6 +59,7 @@ function typeFilter(obj, string) {
     obj.style.color = "white";
     previousClickType = obj;
     projectType = string;
+    console.log(projectType);
     fullFilter();
 }
 
@@ -71,13 +72,13 @@ function languageFilter(obj, string) {
     obj.style.color = "white";
     previousClickLang = obj;
     projectLang = string;
+    console.log(projectLang);
     fullFilter();
 }
 
 // FilteredArray should be const.
 function fullFilter() {
     const filteredArray = [];
-    let m = 0;
     /*
       if(ifDefaultChoices) {
             console.log("default values");
@@ -85,22 +86,19 @@ function fullFilter() {
             //A function might be needed to handle if one of the default values but the other one isn't (type is BOTH lang is JavaScript)
         }
        else {
-        }
+       }
     */
-
-// Use .push(), the M variable isnt neccesary
     for (let i = 0; i < projects.length; i++) {
         if (projects[i].type.includes(projectType) && projects[i].language.includes(projectLang)) {
             filteredArray.push(project(projects[i].name, projects[i].desc, projects[i].type, projects[i].language));
-
-            m++;
         }
     }
     printArray(filteredArray);
 }
-// Same thing here
+
 function printArray(filteredArray) {
     for(let i = 0; i < filteredArray.length; i++) {
-        console.log(filteredArray.name + " " + filteredArray.desc + " " + filteredArray.type + " " + filteredArray.language);
+        console.log(i);
+        console.log(filteredArray[i].name + filteredArray[i].desc + filteredArray[i].type + filteredArray[i].language);
     }
 }
