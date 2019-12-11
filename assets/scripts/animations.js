8c6dceb9eb23d6e8c6cf7c091eb2f4e815ea4864
John Harris <39563774+harrisj09@users.noreply.github.com>
2019-12-11 14:43:01 -0500

    Created vendor.js and animations.js

---
+++ assets/scripts/animations.js
@@ -0,0 +1,7 @@
+//Handles on view animations
+
+/*
+- Grabs view ports of elements targeted.
+- If the distance between between your viewport and the actual location of the element is a certain amount of pxs (or any form of measurement)
+execute the fade in animation.
+ */
---
+++ assets/scripts/vendor.js
@@ -0,0 +1,5 @@
+/*
+Instead of targeting each element again in every class just have a global class that holds them all.
+ */
+
+const projectsWrapper = document.querySelectorAll(".wrapper");
