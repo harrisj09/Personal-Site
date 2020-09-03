55340111b9e1be288771dd712180be7ecdd1fc7a
John Harris <vosm803@gmail.com>
2020-09-02 22:38:20 -0700

    Updated index.js

--- assets/scripts/vendor.js
+++
@@ -1,8 +0,0 @@
-/*
-Instead of targeting each element again in every class just have a global class that holds them all.
- */
-
-    const aboutTitle = document.querySelectorAll(".about__title");
-    const aboutParagraph = document.querySelectorAll(".about__title--paragraph");
-    const projectsWrapper = document.querySelectorAll(".wrapper");
-
